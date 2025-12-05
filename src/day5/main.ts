aedbbc4283ac2179d8a2ac354bc68554aa089d41
Tim Wijma <tim.wijma@gmail.com>
2025-12-05 20:12:29 +0100

    better input parsing day5

--- src/day5/main.ts
+++ src/day5/main.ts
@@ -4,18 +4,14 @@ const input = readFile("input.txt");
 
 const emptyLineIndex = input.findIndex((line) => line.trim() === "");
 
-const ranges = [];
-const ingredients = [];
-for (let i = 0; i < emptyLineIndex; i++) {
-    let line = input[i];
-    ranges.push(line.split("-").map((n) => parseInt(n)));
-}
-for (let i = emptyLineIndex + 1; i < input.length; i++) {
-    ingredients.push(parseInt(input[i]));
-}
+const ranges = input
+    .slice(0, emptyLineIndex)
+    .map((l) => l.split("-").map(Number));
+const ingredients = input.splice(emptyLineIndex + 1).map(Number);
 
 const part1 = () => {
     let fresh = 0;
+
     for (const ingredient of ingredients) {
         for (const [start, end] of ranges) {
             if (ingredient >= start && end >= ingredient) {
@@ -25,32 +21,27 @@ const part1 = () => {
         }
     }
 
-    return fresh
+    return fresh;
 };
 
 const part2 = () => {
-    let s_ranges = [...ranges]
-    s_ranges.sort((a, b) => a[0] - b[0])   
+    let s_ranges = [...ranges];
+    s_ranges.sort((a, b) => a[0] - b[0]);
 
-    const merged = [s_ranges[0]]
+    const merged = [s_ranges[0]];
 
     for (let i = 1; i < s_ranges.length; i++) {
-        const [start, end] = s_ranges[i]
-        const prev = merged[merged.length - 1]
+        const [start, end] = s_ranges[i];
+        const prev = merged[merged.length - 1];
 
         if (start <= prev[1]) {
-            prev[1] = Math.max(prev[1], end)
+            prev[1] = Math.max(prev[1], end);
         } else {
-            merged.push([start, end])
+            merged.push([start, end]);
         }
     }
 
-    let fresh = 0;
-    for (let [start, end] of merged) {
-        fresh += (end - start) + 1
-    }
-
-    return fresh
+    return merged.reduce((sum, [start, end]) => sum + (end - start) + 1, 0);
 };
 
 printResult(part1, part2);
